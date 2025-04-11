4be4b57fc9a3f7a256d83e0ecf49e827c1031c88
Asante Adarkwa Usman <usmanadarkwa@gmail.com>
2025-04-11 09:52:25 -0400

    Added viewmodel to the ProductListScreen and ProductCard for product listing

--- app/src/main/java/com/usman/cvsproductsapp/ui/screens/ProductListScreen.kt
+++ app/src/main/java/com/usman/cvsproductsapp/ui/screens/ProductListScreen.kt
@@ -1,22 +1,64 @@
 package com.usman.cvsproductsapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.usman.cvsproductsapp.ui.vm.ProductListViewModel
+import com.usman.cvsproductsapp.ui.widgets.ProductCard
+import com.usman.cvsproductsapp.utils.UiStatus
+
 
 @Composable
-fun ProductListScreen(navController: NavHostController) {
+fun ProductListScreen(navController: NavHostController, viewModel: ProductListViewModel = hiltViewModel()) {
+
+    val productsUiState by viewModel.products.collectAsState()
+
     Scaffold(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text("This is the product List screen")
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            when (productsUiState) {
+                is UiStatus.Loading -> {
+                    CircularProgressIndicator()
+                }
+                is UiStatus.Success -> {
+                    val productList = (productsUiState as UiStatus.Success).data
+                    // Display the product list
+                    Text("Product List Screen")
+                    Spacer(modifier = Modifier.height(10.dp))
+                    LazyColumn {
+                        items(productList){ product ->
+                            ProductCard(product = product)
+                        }
+                    }
+                }
+                is UiStatus.Error -> {
+                    val errorMessage = (productsUiState as UiStatus.Error).message
+                    Text("Error: $errorMessage")
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/usman/cvsproductsapp/ui/widgets/ProductCard.kt
@@ -0,0 +1,76 @@
+package com.usman.cvsproductsapp.ui.widgets
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.ui.graphics.Color
+import com.usman.cvsproductsapp.data.model.ProductItemModel
+
+@Composable
+fun ProductCard(product: ProductItemModel) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(model = product.image),
+                contentDescription = product.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = product.title,
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = product.description,
+                style = MaterialTheme.typography.bodyMedium
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "$${product.price}",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Rating",
+                    tint = Color.Yellow
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = "${product.rating.rate} (${product.rating.count})")
+            }
+        }
+    }
+}
